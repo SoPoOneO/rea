900be88c9688418a411127177286dba9cea21da7
John Bussey <jbussey365@yahoo.com>
2016-12-12 23:37:07 -0500

    got add buttons mostly working

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,32 @@
 class Roommate extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {name: props.name, expenses: props.expenses};
+        this.addExpense = this.addExpense.bind(this);
+    }
+
+    addExpense(){
+        var key = Date.now();
+        var item = "";
+        var ammount = "";
+        var expense = {key, item, ammount};
+        // console.log(expense);
+        // this.setState((prevState) => ({
+        //     roomate.expenses: prevState.roommate.expenses.concat(expense)
+        // }));
+    }
+
     render() {
         return (
             <li>
-                <input defaultValue={this.props.roommate.name} />
+                <input defaultValue={this.state.name} />
                 <ul>
-                    {this.props.roommate.expenses.map(expense => (
+                    {this.state.expenses.map(expense => (
                         <Expense key={expense.key} expense={expense} />
                     ))}
                 </ul>
-                <button>Add Expense</button>
+                <button onClick={this.addExpense}>Add Expense</button>
                 <br /><br />
             </li>
         );
@@ -38,6 +55,7 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {roommates: roommates};
+        this.addRoommate = this.addRoommate.bind(this);
     }
 
     render() {
@@ -46,12 +64,22 @@ class App extends React.Component {
                 <h3>Roommates</h3>
                 <ul>
                     {this.state.roommates.map(roommate => (
-                        <Roommate key={roommate.key} roommate={roommate} />
+                        <Roommate key={roommate.key} name={roommate.name} expenses={roommate.expenses} />
                     ))}
                 </ul>
-                <button>Add Roommate</button>
+                <button onClick={this.addRoommate}>Add Roommate</button>
             </div>
         );
+    }
+
+    addRoommate(){
+        var key = Date.now();
+        var name = "roommate5";
+        var expenses = [{name: '', ammount: null}];
+        var roommate = {key, name, expenses};
+        this.setState((prevState) => ({
+            roommates: prevState.roommates.concat(roommate)
+        }));
     }
 }
 

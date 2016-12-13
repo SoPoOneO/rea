f9aadb2fdaf5ab5c06cbb4cf4412479b510fbbd9
John Bussey <jbussey365@yahoo.com>
2016-12-12 20:09:15 -0500

    got expenses showing

--- public/js/app.js
+++ public/js/app.js
@@ -9,6 +9,8 @@ class Roommate extends React.Component {
                         <Expense key={expense.key} expense={expense} />
                     ))}
                 </ul>
+                <button>Add Expense</button>
+                <br /><br />
             </li>
         );
     }
@@ -19,7 +21,11 @@ class Expense extends React.Component {
     render() {
         return (
             <li>
-                {this.props.expense.item}: {this.props.expense.ammount}
+                item:
+                <input defaultValue={this.props.expense.item} />
+                -
+                ammout:
+                <input defaultValue={this.props.expense.ammount} />
             </li>
         );
     }
@@ -31,43 +37,45 @@ class App extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            roommates: [{
-                key: 1,
-                name: 'alice',
-                expenses: [{
-                    key: 1,
-                    item: "rent",
-                    ammount: 200
-                },{
-                    key: 2,
-                    item: "food",
-                    ammount: 50
-                }]
-            },{
-                key: 2,
-                name: 'Bob',
-                expenses: [{
-                    key: 1,
-                    item: "laundry",
-                    ammount: 80
-                }]
-            }]
-        };
+        this.state = {roommates: roommates};
     }
 
     render() {
         return (
             <div>
-                <h3>TODO</h3>
+                <h3>Roommates</h3>
                 <ul>
                     {this.state.roommates.map(roommate => (
                         <Roommate key={roommate.key} roommate={roommate} />
                     ))}
                 </ul>
+                <button>Add Roommate</button>
             </div>
         );
     }
 }
+
+var roommates = [{
+        key: 1,
+        name: 'alice',
+        expenses: [{
+            key: 1,
+            item: "rent",
+            ammount: 200
+        },{
+            key: 2,
+            item: "food",
+            ammount: 50
+        }]
+    },{
+        key: 2,
+        name: 'Bob',
+        expenses: [{
+            key: 1,
+            item: "laundry",
+            ammount: 80
+        }]
+    }
+];
 
 ReactDOM.render(<App />,  document.getElementById("content"));
